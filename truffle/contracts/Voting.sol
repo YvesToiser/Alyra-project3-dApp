ce55d2f640671473d6f2d41446b32f8becd34891
YvesToiser <yves.toiser@gmail.com>
2022-07-07 11:19:46 +0200

    fix loop gas limit security breach

--- truffle/contracts/Voting.sol
+++ truffle/contracts/Voting.sol
@@ -1,12 +1,12 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity 0.8.13;
-import "../node_modules/@openzeppelin/contracts/access/Ownable.sol";
-
+import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Voting is Ownable {
 
-    uint public winningProposalID;
+    uint128 public winningProposalID;
+    uint128 highestVoteCount;
 
     struct Voter {
         bool isRegistered;
@@ -32,7 +32,6 @@ contract Voting is Ownable {
     Proposal[] proposalsArray;
     mapping (address => Voter) voters;
 
-
     event VoterRegistered(address voterAddress);
     event WorkflowStatusChange(WorkflowStatus previousStatus, WorkflowStatus newStatus);
     event ProposalRegistered(uint proposalId);
@@ -42,8 +41,6 @@ contract Voting is Ownable {
         require(voters[msg.sender].isRegistered, "You're not a voter");
         _;
     }
-
-    // on peut faire un modifier pour les états
 
     // ::::::::::::: GETTERS ::::::::::::: //
 
@@ -55,7 +52,6 @@ contract Voting is Ownable {
         return proposalsArray[_id];
     }
 
-
     // ::::::::::::: REGISTRATION ::::::::::::: //
 
     function addVoter(address _addr) external onlyOwner {
@@ -65,7 +61,6 @@ contract Voting is Ownable {
         voters[_addr].isRegistered = true;
         emit VoterRegistered(_addr);
     }
-
 
     // ::::::::::::: PROPOSAL ::::::::::::: //
 
@@ -82,7 +77,7 @@ contract Voting is Ownable {
 
     // ::::::::::::: VOTE ::::::::::::: //
 
-    function setVote( uint _id) external onlyVoters {
+    function setVote(uint128 _id) external onlyVoters {
         require(workflowStatus == WorkflowStatus.VotingSessionStarted, 'Voting session havent started yet');
         require(voters[msg.sender].hasVoted != true, 'You have already voted');
         require(_id < proposalsArray.length, 'Proposal not found'); // pas obligé, et pas besoin du >0 car uint
@@ -91,11 +86,16 @@ contract Voting is Ownable {
         voters[msg.sender].hasVoted = true;
         proposalsArray[_id].voteCount++;
 
+        // We set a temporary winning proposal id to prevent making a loop in tally vote and fix security breach
+        if (proposalsArray[_id].voteCount > highestVoteCount) {
+            highestVoteCount = uint128(proposalsArray[_id].voteCount);
+            winningProposalID = _id;
+        }
+
         emit Voted(msg.sender, _id);
     }
 
     // ::::::::::::: STATE ::::::::::::: //
-
 
     function startProposalsRegistering() external onlyOwner {
         require(workflowStatus == WorkflowStatus.RegisteringVoters, 'Registering proposals cant be started now');
@@ -121,17 +121,9 @@ contract Voting is Ownable {
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionStarted, WorkflowStatus.VotingSessionEnded);
     }
 
-
     function tallyVotes() external onlyOwner {
         require(workflowStatus == WorkflowStatus.VotingSessionEnded, "Current status is not voting session ended");
-        uint _winningProposalId;
-        for (uint256 p = 0; p < proposalsArray.length; p++) {
-            if (proposalsArray[p].voteCount > proposalsArray[_winningProposalId].voteCount) {
-                _winningProposalId = p;
-            }
-        }
-        winningProposalID = _winningProposalId;
-
+        // Winning proposal id is already determined by setVote function
         workflowStatus = WorkflowStatus.VotesTallied;
         emit WorkflowStatusChange(WorkflowStatus.VotingSessionEnded, WorkflowStatus.VotesTallied);
     }
